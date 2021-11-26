d1aad7ff43b3448e8b892ef7c5f08a47c7903126
Kirill Vasko <kirill.vasko.2003@gmail.com>
2021-11-26 22:13:28 +0300

    Добавьте файлы проекта.

---
+++ Task_Manager/Core/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Task_Manager.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value;}
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parametr)
+        {
+            return _canExecute == null || _canExecute(parametr);
+        }
+
+        public void Execute(object parametr)
+        {
+            _execute(parametr);
+                
+        }
+       
+    }
+}
